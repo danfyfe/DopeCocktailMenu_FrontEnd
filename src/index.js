75f1835ca886e3dab300586b7b66d808f99a8ce2
axionos <axionoso@gmail.com>
2019-05-28 11:59:13 -0400

    start basic fetch for cocktails

--- src/index.js
+++ src/index.js
@@ -3,7 +3,40 @@
 const moodsEndPoint = 'http://localhost:3000/api/v1/moods'
 const cocktailsEndPoint = 'http://localhost:3000/api/v1/cocktails'
 
+const mainWrapper = document.querySelector("#main-wrapper")
 
+mainWrapper.addEventListener("click", function(e){
+  console.log(e.target)
+  if (e.target.id === 'cozy-btn') {
+
+    fetch(cocktailsEndPoint)
+    .then(resp => resp.json())
+    .then(function(cocktails){
+      // console.log(cocktails)
+
+      cocktails.forEach(function(cocktail){
+        if (cocktail.mood.name === 'Cozy') {
+          console.log(cocktail)
+        }
+      })
+
+    })
+  }
+
+  if (e.target.id === 'sophisticated-btn') {
+
+  }
+
+  if (e.target.id === 'fun-btn') {
+
+  }
+
+  if (e.target.id === 'classic-btn') {
+
+  }
+})
+
+// // FETCH EACH MOOD
 // fetch(moodsEndPoint)
 // .then(resp=>resp.json())
 // .then(function(moods){
@@ -11,9 +44,5 @@ const cocktailsEndPoint = 'http://localhost:3000/api/v1/cocktails'
 // })
 //
 //
-// fetch(cocktailsEndPoint)
-// .then(resp=>resp.json())
-// .then(function(cocktails){
-//   console.log(cocktails)
-// })
 
+// })
