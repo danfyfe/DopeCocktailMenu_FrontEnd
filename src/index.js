d3a553e1aeebf16c1b9404c1d674a03d931498b8
danfyfe <fyfedaniel@gmail.com>
2019-05-28 11:04:32 -0400

    start front end

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 // javascript is the best!
+
+const moodsEndPoint = 'http://localhost:3000/api/v1/moods'
+const cocktailsEndPoint = 'http://localhost:3000/api/v1/cocktails'
+
+fetch(moodsEndPoint)
+.then(resp=>resp.json())
+.then(function(moods){
+  console.log(moods)
+})
+
+
+fetch(cocktailsEndPoint)
+.then(resp=>resp.json())
+.then(function(cocktails){
+  console.log(cocktails)
+})
+
